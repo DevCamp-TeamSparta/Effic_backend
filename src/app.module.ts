ba10460bb777abd43dd7f6691496a4474f36d200
hclo99 <junghs710@naver.com>
2023-08-07 23:17:42 +0900

    feat: dotenv & config 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigValidator } from "../config/db.config";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(ConfigValidator)],
   controllers: [AppController],
   providers: [AppService],
 })
