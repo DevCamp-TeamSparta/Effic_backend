ce082ec1c26df4ed12c62ead9e16f83d8b8b19e7
hclo99 <junghs710@naver.com>
2023-08-08 10:03:45 +0900

    feat: 기본 로그인 api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { ConfigValidator } from "../config/db.config";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CongfigValidator } from '../config/db.config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MessageModule } from './messages/messages.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ConfigValidator)],
+  imports: [
+    TypeOrmModule.forRoot(CongfigValidator),
+    MessageModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
