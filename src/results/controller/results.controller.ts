103ec389efc6064c1ad55faebb4ce3ebc3fb0a1e
hclo99 <junghs710@naver.com>
2023-08-23 14:33:14 +0900

    feat: 자잘한 수정

--- src/results/controller/results.controller.ts
+++ src/results/controller/results.controller.ts
@@ -12,18 +12,18 @@ export class ResultsController {
     return await this.resultsService.messageResult(messageId);
   }
 
-  @Get('/default/:messageId')
-  async defaultMessageResult(@Param('messageId') messageId: number) {
-    this.logger.verbose('Default message result');
-    return await this.resultsService.shortUrlResult(messageId);
-  }
+  // @Get('/default/:messageId')
+  // async defaultMessageResult(@Param('messageId') messageId: number) {
+  //   this.logger.verbose('Default message result');
+  //   return await this.resultsService.shortUrlResult(messageId);
+  // }
 
-  @Get('/default/ncp/:messageId')
-  async defaultMessageNcpResult(
-    @Param('messageId') messageId: number,
-    @Headers('email') headerEmail: string,
-  ) {
-    this.logger.verbose('Default message ncp result');
-    return await this.resultsService.ncpResult(messageId, headerEmail);
-  }
+  // @Get('/default/ncp/:messageId')
+  // async defaultMessageNcpResult(
+  //   @Param('messageId') messageId: number,
+  //   @Headers('email') headerEmail: string,
+  // ) {
+  //   this.logger.verbose('Default message ncp result');
+  //   return await this.resultsService.ncpResult(messageId, headerEmail);
+  // }
 }
