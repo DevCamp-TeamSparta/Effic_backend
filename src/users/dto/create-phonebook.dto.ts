86f059403dd8a90fa5856a1d9b2ffd763e898620
hclo99 <junghs710@naver.com>
2023-09-14 11:24:18 +0900

    feat: 주소록 생성 중

--- src/users/dto/create-phonebook.dto.ts
+++ src/users/dto/create-phonebook.dto.ts
@@ -1,4 +1,4 @@
-import { IsArray, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsArray, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreatePhonebookDto {
   @IsString()
@@ -7,9 +7,5 @@ export class CreatePhonebookDto {
 
   @IsArray()
   @IsNotEmpty()
-  member: string[];
-
-  @IsArray()
-  @IsOptional()
-  variableKey: string[];
+  members: string[];
 }
