b87d23f2655c0a9eedf3eba750cc1140f4d2e0a3
hclo99 <junghs710@naver.com>
2023-09-19 17:52:48 +0900

    feat: bizmessage 생성 중

--- src/bizmessage/controller/bizmessage.controller.ts
+++ src/bizmessage/controller/bizmessage.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger, Post, Headers } from '@nestjs/common';
+import { BizmessageService } from '../service/bizmessage.service';
+import * as jwt from 'jsonwebtoken';
 
 @Controller('bizmessage')
-export class BizmessageController {}
+export class BizmessageController {
+  private logger = new Logger('BizmessageController');
+  constructor(private bizmessageService: BizmessageService) {}
+
+  // 기본 친구톡 보내기
+  @Post('/default')
+  async sendDefaultBizmessage(@Headers('Authorization') authorization: string) {
+    this.logger.verbose('Default bizmessage sending');
+
+    const accessToken = authorization.split(' ')[1];
+    const decodedAccessToken: any = jwt.decode(accessToken);
+
+    const email = decodedAccessToken.email;
+  }
+}
