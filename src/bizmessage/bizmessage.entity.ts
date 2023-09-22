9bd8376356863b01b24e364b7c8f7e0591534491
hclo99 <junghs710@naver.com>
2023-09-22 12:15:31 +0900

    feat: 친구톡 defualt 생성 중

--- src/bizmessage/bizmessage.entity.ts
+++ src/bizmessage/bizmessage.entity.ts
@@ -1,7 +1,27 @@
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { bizmessageType } from './bizmessage.enum';
 
 @Entity()
 export class Bizmessage extends BaseEntity {
   @PrimaryGeneratedColumn({ type: 'int' })
   bizmessageId: number;
+
+  @Column({ type: 'boolean', nullable: false, default: false })
+  isSent: boolean;
+
+  @Column()
+  sentTpye: bizmessageType;
+
+  @Column({ array: true, nullable: true, type: 'text', default: [] })
+  idString: Array<string>;
+
+  @Column({ array: true, nullable: true, type: 'text', default: [] })
+  receiverList: string[];
+
+  @Column({
+    type: 'timestamp',
+    nullable: false,
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
 }
