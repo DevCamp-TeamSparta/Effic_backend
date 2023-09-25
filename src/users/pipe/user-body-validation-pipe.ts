5dd96241755ede8447aa68e390d4e87c58b3c6e8
hclo99 <junghs710@naver.com>
2023-09-25 14:21:50 +0900

    feat: hostnumber undefined 경우 추가

--- src/users/pipe/user-body-validation-pipe.ts
+++ src/users/pipe/user-body-validation-pipe.ts
@@ -9,6 +9,11 @@ export class UserBodyValidationPipe implements PipeTransform {
   async transform(value: any): Promise<UpdateUserDto | CreateUserDto> {
     const hostnumberRegex = /^\d{7,11}$/;
 
+    // hostnumber가 undefined이면 에러 추가
+    if (value.hostnumber === undefined) {
+      throw new BadRequestException('hostnumber is not defined');
+    }
+
     if (
       value.hostnumber.length &&
       value.hostnumber.every((n) => !hostnumberRegex.test(n))
