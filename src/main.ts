9c8724b21adffb40e9184b2f9489b4d84096ce56
Sang Un Lee <sangunlee6@gmail.com>
2023-11-30 15:46:37 +0900

    Chore: 서버 포트 변경
    - 기존 8000번 포트에서 => 3000번 포트로 변경

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
 
-  await app.listen(8000);
+  await app.listen(3000);
 }
 bootstrap();
