a8949e690841292eba264752b7e90056d9e070bc
Sang Un Lee <sangunlee6@gmail.com>
2023-12-22 15:14:07 +0900

    Feat: POST, /filter-query/fatigue-level API의 DTO 유효성 검증

--- src/segment/application/port/in/dto/create-filter-query-by-fatigue-level.dto.ts
+++ src/segment/application/port/in/dto/create-filter-query-by-fatigue-level.dto.ts
@@ -1,5 +1,15 @@
-export interface CreateFilterQueryByFatigueLevelDto {
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateFilterQueryByFatigueLevelDto {
+  @IsNumber()
+  @IsNotEmpty()
   segmentId: number;
+
+  @IsString()
+  @IsNotEmpty()
   receiverNumberColumnName: string;
+
+  @IsNumber()
+  @IsNotEmpty()
   fatigueLevelDays: number;
 }
