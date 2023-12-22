29e4e6d11308f931ebd9dd2535cdfc1fca04f4a8
Sang Un Lee <sangunlee6@gmail.com>
2023-12-22 15:16:08 +0900

    Feat: POST, /filter-query/variable-value API의 DTO 유효성 검증

--- src/segment/application/port/in/dto/create-filter-query-by-variable-value.dto.ts
+++ src/segment/application/port/in/dto/create-filter-query-by-variable-value.dto.ts
@@ -1,6 +1,19 @@
-export interface CreateFilterQueryByVariableValueDto {
+import { IsBoolean, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateFilterQueryByVariableValueDto {
+  @IsNotEmpty()
+  @IsNumber()
   segmentId: number;
+
+  @IsNotEmpty()
+  @IsString()
   columnName: string;
+
+  @IsNotEmpty()
+  @IsString()
   value: string;
+
+  @IsNotEmpty()
+  @IsBoolean()
   excludeValue: boolean;
 }
