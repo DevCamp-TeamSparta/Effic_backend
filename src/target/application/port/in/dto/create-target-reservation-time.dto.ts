497e8d80708e3e04a9e132e9cbe977fa27999487
Sang Un Lee <sangunlee6@gmail.com>
2023-12-22 15:34:39 +0900

    Feat: POST, /target/reservation-time API의 DTO 유효성 검증

--- src/target/application/port/in/dto/create-target-reservation-time.dto.ts
+++ src/target/application/port/in/dto/create-target-reservation-time.dto.ts
@@ -1,11 +1,44 @@
-export interface CreateTargetReservationTimeDto {
+import {
+  ArrayNotEmpty,
+  IsBoolean,
+  IsDate,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+} from 'class-validator';
+
+export class CreateTargetReservationTimeDto {
+  @IsNotEmpty()
+  @ArrayNotEmpty()
+  @IsNumber({}, { each: true })
   targetIds: number[];
+
+  @IsNotEmpty()
+  @IsNumber()
   segmentId: number;
+
+  @IsNotEmpty()
+  @IsString()
   timeColumnName: string;
+
+  @IsNotEmpty()
+  @IsString()
   receiverNumberColumnName: string;
+
+  @IsNotEmpty()
+  @IsString()
   delayDays: number;
+
+  @IsNotEmpty()
+  @IsDate()
   reservationTime: Date;
+
+  @IsDate()
   endDate?: Date;
+
+  @IsBoolean()
   isRecurring?: boolean;
+
+  @IsString({ each: true })
   weekDays?: string[];
 }
