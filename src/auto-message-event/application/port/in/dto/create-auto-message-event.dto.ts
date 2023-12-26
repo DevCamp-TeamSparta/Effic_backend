3375c49e8fac1660b26cc4f66c02c06d9acf0af2
Sang Un Lee <sangunlee6@gmail.com>
2023-12-26 09:53:13 +0900

    Refactor: auto-message-event-dto 유효성 검증 추가

--- src/auto-message-event/application/port/in/dto/create-auto-message-event.dto.ts
+++ src/auto-message-event/application/port/in/dto/create-auto-message-event.dto.ts
@@ -1,9 +1,20 @@
-export interface CreateAutoMessageEventDto {
+import { IsBoolean, IsDate, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateAutoMessageEventDto {
+  @IsString()
+  @IsNotEmpty()
   autoMessageEventName: string;
+
   totalSentCount?: number | null;
   clickCount?: number | null;
   clickRate?: number | null;
+
+  @IsDate()
   scheduledEndDate: Date;
+
+  @IsDate()
   createdDate: Date;
+
+  @IsBoolean()
   isActive: boolean;
 }
--- src/auto-message-event/application/port/in/dto/update-auto-message-event.dto.ts
+++ src/auto-message-event/application/port/in/dto/update-auto-message-event.dto.ts
@@ -1,10 +1,29 @@
-export interface UpdateAutoMessageEventDto {
+import {
+  IsBoolean,
+  IsDate,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+} from 'class-validator';
+
+export class UpdateAutoMessageEventDto {
+  @IsNumber()
+  @IsNotEmpty()
   autoMessageEventId: number;
+
+  @IsString()
   autoMessageEventName?: string;
+
   totalSentCount?: number | null;
   clickCount?: number | null;
   clickRate?: number | null;
+
+  @IsDate()
   scheduledEndDate?: Date;
+
+  @IsDate()
   createdDate?: Date;
+
+  @IsBoolean()
   isActive?: boolean;
 }
