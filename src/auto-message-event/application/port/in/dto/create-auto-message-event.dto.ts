285c127ee9f172e8b972c435597ea3441e5bc455
Sang Un Lee <sangunlee6@gmail.com>
2023-12-27 14:36:03 +0900

    Fix: CreateAutoMessageEventDto 유효성 검사 수정

--- src/auto-message-event/application/port/in/dto/create-auto-message-event.dto.ts
+++ src/auto-message-event/application/port/in/dto/create-auto-message-event.dto.ts
@@ -1,17 +1,35 @@
-import { IsBoolean, IsDate, IsNotEmpty, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+import {
+  IsBoolean,
+  IsDate,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
 export class CreateAutoMessageEventDto {
   @IsString()
   @IsNotEmpty()
   autoMessageEventName: string;
 
+  @IsNumber()
+  @IsOptional()
   totalSentCount?: number | null;
+
+  @IsNumber()
+  @IsOptional()
   clickCount?: number | null;
+
+  @IsNumber()
+  @IsOptional()
   clickRate?: number | null;
 
+  @Type(() => Date)
   @IsDate()
   scheduledEndDate: Date;
 
+  @Type(() => Date)
   @IsDate()
   createdDate: Date;
 
